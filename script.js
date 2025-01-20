8740cdd6fd4075994bd1f0bb74ca830cf1a5ef3e
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-01-20 23:46:59 +0530

    adding full function JS

--- script.js
+++ script.js
@@ -1,4 +1,47 @@
-const rock = document.getElementById('rock');
-const paper = document.getElementById('paper');
-const scissors = document.getElementById('scissors');
-const result = document.getElementById('result');
+const choices = ["rock", "paper", "scissors"];
+const playerResult = document.getElementById("playerResult");
+const computerResult = document.getElementById("computerResult");
+const displayResult = document.getElementById("displayResult");
+
+function playerChoice(choice) {
+
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = "";
+
+    if (choice === computerChoice) {
+        result = "It's a tie!";
+    }
+    else {
+        switch (choice) {
+            case "rock":
+                result = computerChoice === "scissors" ? "You win!" : "You lose!";
+                break;
+            case "paper":
+                result = computerChoice === "rock" ? "You win!" : "You lose!";
+                break;
+            case "scissors":
+                result = computerChoice === "paper" ? "You win!" : "You lose!";
+                break;
+        }
+    }
+
+    playerResult.textContent = `Player chose: ${choice}`;
+    computerResult.textContent = `Computer chose: ${computerChoice}`;
+    displayResult.innerHTML = result;
+
+    displayResult.classList.remove("greenText", "redText", "yellowText");
+
+    switch (result) {
+        case "You win!":
+            displayResult.classList.add("greenText");
+            break;
+        case "You lose!":
+            displayResult.classList.add("redText");
+            break;
+        case "It's a tie!":
+            displayResult.classList.add("yellowText");
+            break;
+    }
+
+
+}

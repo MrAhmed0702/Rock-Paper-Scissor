deb8d0acaf7aea55c9145d7d2748883d3344cc59
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-01-20 23:21:06 +0530

    Rock-Paper-Scissors

---
+++ script.js
@@ -0,0 +1,4 @@
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const result = document.getElementById('result');
